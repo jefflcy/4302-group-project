ad2b5bad2986ab527cadf41335d8d9337d53f635
jepp <108892550+jefflcy@users.noreply.github.com>
2024-04-12 17:02:11 +0800

    minor edits

--- contracts/VolunteerToken.sol
+++ contracts/VolunteerToken.sol
@@ -1,6 +1,6 @@
 // contracts/VolunteerToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -10,7 +10,7 @@ import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 contract VolunteerToken is ERC1155, Ownable {
 
     // baseURI: https://ipfs.io/ipfs/QmXHGAwVWFFstAHTX758FE5eiEb7TghFnUN3xfQCu2dk6B/
-    string baseURI;
+    string private _baseURI;
 
     // maps `projId` to `mintedSuply`
     mapping(uint256 => uint256) projectsHistory;
@@ -21,7 +21,7 @@ contract VolunteerToken is ERC1155, Ownable {
     // _uri: https://ipfs.io/ipfs/QmXHGAwVWFFstAHTX758FE5eiEb7TghFnUN3xfQCu2dk6B/
     // owner: the deployer of the contract (input when calling from Volunteer.sol)
     constructor(string memory _uri) ERC1155(_uri) Ownable(tx.origin) {
-        baseURI = _uri;
+        _baseURI = _uri;
     } 
 
     // mint nfts to the Volunteer.sol CA
@@ -47,13 +47,13 @@ contract VolunteerToken is ERC1155, Ownable {
     }
 
     // override URI for indiv project metadata uri
-    function uri(uint256 _projId) public view override returns (string memory) {
-        return string(abi.encodePacked(baseURI, Strings.toString(_projId), ".json"));
+    function uri(uint256 projId) public view override returns (string memory) {
+        return string(abi.encodePacked(_baseURI, Strings.toString(projId), ".json"));
     }
 
     // URI for entire contract for opensea
     function contractURI() public view returns (string memory) {
-        return string(abi.encodePacked(baseURI, "collection.json"));
+        return string(abi.encodePacked(_baseURI, "collection.json"));
     }
 
     // making it soulbound, ie. only owner can transfer/burn tokens
