1e92947f26b328643b4dd083ab537cabca536a80
jepp <108892550+jefflcy@users.noreply.github.com>
2024-04-17 17:36:29 +0800

    add migrations script and test script template

--- migrations/deploy_contract.js
+++
@@ -1,7 +0,0 @@
-const Volunteer = artifacts.require("Volunteer");
-const VolunteerToken = artifacts.require("VolunteerToken");
-
-module.exports = (deployer, network, accounts) => {
-  deployer.deploy(Volunteer);
-  deployer.deploy(VolunteerToken);
-};
--- test/test_volunteer.js
+++ test/test_volunteer.js
@@ -1,6 +1,6 @@
-const Volunteer = artifacts.require("Volunteer");
-const VolunteerToken = artifacts.require("VolunteerToken");
-const _deploy_contracts = require("../migrations/deploy_contracts");
+const Volunteer = artifacts.require("../contracts/Volunteer.sol");
+const VolunteerToken = artifacts.require("../contracts/VolunteerToken.sol");
+const _deploy_contracts = require("../migrations/2_deploy_contracts");
 var assert = require("assert");
 // may need bignumber for testing
 // may need truffleassertions for testing
@@ -8,10 +8,27 @@ var assert = require("assert");
 
 /* INSERT UNIT TESTS BELOW */
 
-contract("Volunteer", async (accounts) => {
-  it("Should return the correct first projId", async function () {
-    let volunteerInstance = await Volunteer.deployed();
-    let firstProjId = await volunteerInstance.getNextProjId();
-    assert.equal(firstProjId, 0);
+contract("Volunteer", (accounts) => {
+  before(async () => {
+    volunteerInstance = await Volunteer.new(
+      "https://ipfs.io/ipfs/QmXHGAwVWFFstAHTX758FE5eiEb7TghFnUN3xfQCu2dk6B/"
+    );
   });
+  console.log("Testing Volunteer Contracts");
+
+  // ------------------- Test cases -------------------
+
+  // functions below not tested/working yet
+
+  //   it("should create a new VolunteerToken on deployment", async function () {
+  //     const tokenAddress = await volunteerInstance.getVolunteerTokenAddress(); // Method to get the deployed token address
+  //     const tokenInstance = await VolunteerToken.at(tokenAddress);
+
+  //     assert(tokenInstance !== undefined, "VolunteerToken instance should exist");
+  //   });
+
+  //   it("Should return the correct first projId", async () => {
+  //     let firstProjId = await volunteerInstance.getNextProjId();
+  //     assert.equal(firstProjId, 0);
+  //   });
 });
