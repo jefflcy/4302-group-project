d338a49388481a33ce0a71e1c29700049d8346be
jepp <108892550+jefflcy@users.noreply.github.com>
2024-04-18 17:04:43 +0800

    cleanup migrations and added unit tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,5 +3,5 @@ const Volunteer = artifacts.require("../contracts/Volunteer.sol");
 module.exports = (deployer, network, accounts) => {
   const tokenUri =
     "https://ipfs.io/ipfs/QmXHGAwVWFFstAHTX758FE5eiEb7TghFnUN3xfQCu2dk6B/";
-  deployer.deploy(Volunteer, tokenUri);
+  deployer.deploy(Volunteer, tokenUri, { from: accounts[0] });
 };
--- test/test_volunteer.js
+++ test/test_volunteer.js
@@ -1,6 +1,8 @@
 const Volunteer = artifacts.require("../contracts/Volunteer.sol");
 const VolunteerToken = artifacts.require("../contracts/VolunteerToken.sol");
 const _deploy_contracts = require("../migrations/2_deploy_contracts");
+const { expectRevertCustomError } = require("custom-error-test-helper"); // package to test for custom error messages
+
 var assert = require("assert");
 // may need bignumber for testing
 // may need truffleassertions for testing
@@ -10,25 +12,31 @@ var assert = require("assert");
 
 contract("Volunteer", (accounts) => {
   before(async () => {
-    volunteerInstance = await Volunteer.new(
-      "https://ipfs.io/ipfs/QmXHGAwVWFFstAHTX758FE5eiEb7TghFnUN3xfQCu2dk6B/"
-    );
+    volunteerInstance = await Volunteer.deployed();
   });
-  console.log("Testing Volunteer Contracts");
+  console.log("Testing Volunteer and VolunteerToken contracts");
 
   // ------------------- Test cases -------------------
 
-  // functions below not tested/working yet
+  it("should create a new VolunteerToken on deployment", async function () {
+    const tokenAddress = await volunteerInstance.getVolunteerTokenAddress(); // Method to get the deployed token address
+    const tokenInstance = await VolunteerToken.at(tokenAddress);
+    // console.log("Token Address: ", tokenAddress); // for testing
+    assert(tokenInstance !== undefined, "VolunteerToken instance should exist");
+  });
 
-  //   it("should create a new VolunteerToken on deployment", async function () {
-  //     const tokenAddress = await volunteerInstance.getVolunteerTokenAddress(); // Method to get the deployed token address
-  //     const tokenInstance = await VolunteerToken.at(tokenAddress);
+  it("Should return the correct first projId", async () => {
+    let firstProjId = await volunteerInstance.getNextProjId();
+    assert.equal(firstProjId, 0);
+  });
 
-  //     assert(tokenInstance !== undefined, "VolunteerToken instance should exist");
-  //   });
-
-  //   it("Should return the correct first projId", async () => {
-  //     let firstProjId = await volunteerInstance.getNextProjId();
-  //     assert.equal(firstProjId, 0);
-  //   });
+  it("Should restrict createProject to only the owner", async () => {
+    await expectRevertCustomError(
+      Volunteer,
+      volunteerInstance.createProject(1713400581, 1713486981, {
+        from: accounts[1],
+      }),
+      "OwnableUnauthorizedAccount"
+    );
+  });
 });
