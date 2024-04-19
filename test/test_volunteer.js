5bc628f6ba4d90792f6f1332b6c19f216965b3d2
Javian Ng <j4vianz01@gmail.com>
2024-04-20 01:20:15 +0800

    Revert "commit"
    This reverts commit 5e531f33af08615d4df87897050c48ad34fdfd7f.

--- test/test_volunteer.js
+++ test/test_volunteer.js
@@ -5,12 +5,9 @@ const { expectRevertCustomError } = require("custom-error-test-helper"); // pack
 
 var assert = require("assert");
 const truffleAssert = require('truffle-assertions');
-const truffleAssert = require('truffle-assertions');
 // may need bignumber for testing
 // may need truffleassertions for testing
 // may need moment.js for datetime or use Date(): Math.floor(new Date().getTime() / 1000)
-const {time, expectRevert} = require('@openzeppelin/test-helpers'); // npm install @openzeppelin/test-helpers
-const { startTimeAfter, startTimePrior, endTimeAfter } = require('./helper');
 
 /* INSERT UNIT TESTS BELOW */
 
